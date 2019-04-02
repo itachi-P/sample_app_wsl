32032cdd935e40431493e5995aee056f0b27d0ee
itachi-P <itachip38@gmail.com>
2019-04-02 22:52:56 +0900

    :no_good: User registration failure processing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # 以降privateであることを明示する為にネストを一段深くするのは有用
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+
 end
