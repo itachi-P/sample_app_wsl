a9e5701d96896c6382927586d5aecdfc5ff80f39
itachi-P <itachip38@gmail.com>
2019-04-11 02:34:52 +0900

    :lion: Add user edit/update action and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
-
   def show
     @user = User.find(params[:id])
-    #debugger
+    # debugger
   end
 
   def new
@@ -13,19 +12,23 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       log_in @user
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = 'Welcome to the Sample App!'
+      # 下記は redirect_to user_url(@user)と同じと解釈される
       redirect_to @user
-      # 上記は redirect_to user_url(@user)と同じと解釈される
     else
       render 'new'
     end
   end
 
-  private
-    # 以降privateであることを明示する為にネストを一段深くするのは有用
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  # 以降の定義がprivateであることを明示する為にネストを一段深くする
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
